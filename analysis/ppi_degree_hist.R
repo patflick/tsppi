5116668fe50c23e40d778b011754d21a7e41a46b
U-NET\flick <patrick.flick@gmail.com>
2013-03-25 17:19:53 +0100

    further degree distribution analysis

--- analysis/ppi_degree_hist.R
+++ analysis/ppi_degree_hist.R
@@ -44,29 +44,43 @@ ORDER BY CountHigh+CountMedium+CountLow ASC
 ")
 
 
+dbDisconnect(con)
 
+
+# calculate the power law
+library(igraph)
+powerlaw <- power.law.fit(data$Degree)
+print(powerlaw)
 
 
 
 # save as png
-png("degree_distr.png", height=800, width=1024)
+#png("degree_distr.png", height=1050, width=1680)
 
 par(mfrow=c(2,2))
 
-thresholds<-c(15,30,45,60)
+thresholds<-c(0.2,0.4,0.6,0.8)
 
 for (threshold in thresholds) {
-	degrees_high=data$Degree[which(data$CountLow+data$CountMedium+data$CountHigh > threshold)]
-	degrees_low=data$Degree[which(data$CountLow+data$CountMedium+data$CountHigh <= threshold)]
-	plot(density(degrees_high), col="blue", main=paste(c("Degree distribution for threshold ", threshold),sep=""), xlab="Node degree")
-	lines(density(degrees_low),col="red")
+	n <- nrow(data)
+	degrees_high = data$Degree[1:floor(threshold*n)]
+	degrees_low = data$Degree[(floor(threshold*n)+1):n]
+	hist_high = hist(degrees_high, plot=FALSE,breaks=max(degrees_high))
+	hist_low = hist(degrees_low, plot=FALSE,breaks=max(degrees_low))
+	y1 = c(0,hist_low$density,0)
+	y2 = c(0,hist_high$density,0) 
+	plot(c(0,hist_low$breaks), y1 ,type="s", col="blue", xlim= c(0,max(c(hist_low$breaks,hist_high$breaks))),ylim= c(0,max(c(y1,y2))),main=paste(c("Degree distribution for threshold ", threshold),sep=""), xlab="Node degree", ylab="Density")
+	lines(x=c(0,hist_high$breaks), y = y2, type="s", col="red")
 	legend("topright",legend=c("High Specificity", "Low Specificity"), fill=c("red","blue"))
+	pl_high <- power.law.fit(degrees_high)
+	pl_low <- power.law.fit(degrees_low)
+	text(100,0.4, paste(c("High Specificity\nPower Law: ",pl_high$alpha, "\nLow Specificity\nPower Law: ", pl_low$alpha), sep="", collapse=""), pos=4)
 }
 
 
 
 
-dbDisconnect(con)
+
 
 # close png output
-dev.off()
+#dev.off()
