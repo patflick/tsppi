ce28bce87f0306be6678098c4a1c0ccf2ae9d447
r4d2 <patrick.flick@gmail.com>
2014-01-22 22:56:30 -0500

    more on the analysis of bossi & lehner

--- analysis/bossi_lehner.R
+++ analysis/bossi_lehner.R
@@ -3,6 +3,7 @@
 library(ggplot2)
 library(plyr) # for `round_any`
 library(gridExtra) # for `grid.arrange`
+library(Hmisc) # for spearman correlation + p-value
 
 # returns a list of all PPIs that can be analyzed
 # TODO: put this as a unified function somewhere (using the SQL database)
@@ -20,10 +21,28 @@ get_exprs <- function()
     return(exprs)
 }
 
+###########################################
+#  functions for calculating error bars:  #
+###########################################
 
-# reproducing the Bossi&Lehner plot (Fig. 1 B)
-plot_degree_vs_expr <- function(ppi_name="bossi",expr_name="gene_atlas",prop_name="maxts_degree")
+
+# standard error:
+std <- function(x) sd(x)/sqrt(length(x))
+
+# mean plus/minus one standard error
+upper_se <- function(x) mean(x) + std(x)
+lower_se <- function(x) mean(x) - std(x)
+
+
+
+##########################
+#  Bossi & Lehner Fig1B  #
+##########################
+
+
+get_expression_property_data <- function(ppi_name="bossi",expr_name="gene_atlas",prop_name="degree_test")
 {
+
     # load sql config and get connection
     source("sql_config.R")
     con <- get_sql_conn('/home/patrick/dev/bio/data/test_matching.sqlite')
@@ -40,6 +59,15 @@ plot_degree_vs_expr <- function(ppi_name="bossi",expr_name="gene_atlas",prop_nam
 
     # load ppi network from db
     data <- dbGetQuery(con, query)
+    return(data);
+}
+
+
+# reproducing the Bossi&Lehner plot (Fig. 1 B)
+plot_degree_vs_expr <- function(ppi_name="bossi",expr_name="gene_atlas",prop_name="maxts_degree")
+{
+
+    data <- get_expression_property_data(ppi_name, expr_name, prop_name)
 
     # round off the exressedcount as simple "binning"
     # TODO: might be able to do this better :)
@@ -49,16 +77,11 @@ plot_degree_vs_expr <- function(ppi_name="bossi",expr_name="gene_atlas",prop_nam
     {
         round_to <- round(max_expr_count / 7)
     }
-    data$ExpressedCount <- round_any(data$ExpressedCount, round_to)
+    #data$ExpressedCount <- round_any(data$ExpressedCount, round_to)
 
     error_width <- max_expr_count / 20
 
-    # standard error:
-    std <- function(x) sd(x)/sqrt(length(x))
-
-    # mean plus/minus one standard error
-    upper_se <- function(x) mean(x) + std(x)
-    lower_se <- function(x) mean(x) - std(x)
+    print(paste(ppi_name, expr_name,":",length(data$ExpressedCount)))
 
     # plot using ggplot2
     fig <- ggplot(data, aes(x=ExpressedCount, y=Value)) +
@@ -75,23 +98,113 @@ plot_degree_vs_expr <- function(ppi_name="bossi",expr_name="gene_atlas",prop_nam
 }
 
 
-ppis <- get_ppis()
-exprs <- get_exprs()
-
-par(mfrow=c(length(ppis),length(exprs)))
-
-figs <- list()
-for (p in ppis)
+# checking for bossi results:
+# 1.) tissue specific proteins make fewer protein interactions than widely
+#     expressed proteins (use spearman)
+# 2.) most recently evolved proteins have fewer interactions than ancient proteins TODO
+test_bossi_1 <- function(ppi_name="bossi", expr_name="gene_atlas", prop_name="maxts_degree")
 {
-    for (e in exprs)
+    data <- get_expression_property_data(ppi_name, expr_name, prop_name)
+
+    # run a spearman test:
+
+    for (test in c("spearman", "kendall", "pearson"))
     {
-        # call the bossi plotting function
-        fig <- plot_degree_vs_expr(p, e, "coexpr_degree")
-        figs <- c(figs, list(fig))
+        tst = cor.test(data$ExpressedCount, data$Value, method=test)
+        rho = tst$estimate
+        pvalue = tst$p.value
+        print(paste("runing", test, "test: ", ppi_name, expr_name, "rho=", rho, ", p-value=", pvalue))
     }
 }
 
-do.call(grid.arrange, figs)
-#grid.arrange(figs, ncol=length(exprs))
+# 3.) to which extend to tissue-specific proteins interact with most widely expressed proteins?
+test_bossi_3 <- function(ppi_name="bossi", expr_name="gene_atlas", prop_name="maxts_degree")
+{
+    # needs data from the actual graph -> TODO implement in NetworKit!?
+}
+
+
+create_test_degrees <- function(ppi_name="bossi",expr_name="gene_atlas",prop_name="degree_test")
+{
+    
+    source("sql_config.R")
+    con <- get_sql_conn('/home/patrick/dev/bio/data/test_matching.sqlite')
+
+    # create table names
+    prop_table <- paste("prop", ppi_name, expr_name, prop_name, sep="_")
+    
+    dbSendQuery(con, paste("DROP TABLE IF EXISTS ", prop_table))
+    query <- paste("CREATE TABLE IF NOT EXISTS ", prop_table, " AS ",
+                   "SELECT Gene, COUNT(*) AS Value FROM ",
+                   "(",
+                   "SELECT Gene1 AS Gene FROM ", ppi_name,
+                   # requires UNION all, otherwise all degrees will be 1
+                   " UNION ALL ",
+                   "SELECT Gene2 As Gene FROM ", ppi_name,
+                   ")",
+                   "GROUP BY Gene")
+    dbSendQuery(con, query);
+}
+
+
+compare_test_degrees <- function(ppi_name="bossi",expr_name="gene_atlas",prop_name="degree_test")
+{
+    
+    source("sql_config.R")
+    con <- get_sql_conn('/home/patrick/dev/bio/data/test_matching.sqlite')
+
+    # create table names
+    prop_table_1 <- paste("prop", ppi_name, expr_name, "_degree", sep="_")
+    prop_table_2 <- paste("prop", ppi_name, expr_name, "degree_test", sep="_")
+
+    query <- paste("SELECT a.Gene, a.Value AS Value1, b.Value AS Value2 ",
+                   "FROM ", prop_table_1, " AS a INNER JOIN ",
+                   prop_table_2, " AS b ON a.Gene = b.Gene")
+    data <- dbGetQuery(con, query)
+
+    #print(which(data$Value1 != data$Value2))
+}
+
+test_for_all <- function(func)
+{
+    ppis <- get_ppis()
+    exprs <- get_exprs()
+    for (p in ppis)
+    {
+        for (e in exprs)
+        {
+            func(p,e)
+        }
+    }
+}
+
+
+plot_all <- function()
+{
+    ppis <- get_ppis()
+    exprs <- get_exprs()
+
+    par(mfrow=c(length(ppis),length(exprs)))
+
+    figs <- list()
+    for (p in ppis)
+    {
+        for (e in exprs)
+        {
+            # call the bossi plotting function
+            #fig <- plot_degree_vs_expr(p, e, "coexpr_degree")
+            create_test_degrees(p,e);
+            fig <- plot_degree_vs_expr(p, e, "degree_test")
+            figs <- c(figs, list(fig))
+        }
+    }
+
+    all_figs <- do.call(grid.arrange, figs)
+
+    # in order to pass named parameters to grid.arrange:
+    #do.call(grid.arrange, c(figs,list(nrow=5,left="hello there"))
+}
+
+
 
 
