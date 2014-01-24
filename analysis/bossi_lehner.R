6aad251042fcd1db2e9981041275ab955ba7bb88
r4d2 <patrick.flick@gmail.com>
2014-01-23 23:48:20 -0500

    more for analysis

--- analysis/bossi_lehner.R
+++ analysis/bossi_lehner.R
@@ -77,7 +77,9 @@ plot_degree_vs_expr <- function(ppi_name="bossi",expr_name="gene_atlas",prop_nam
     {
         round_to <- round(max_expr_count / 7)
     }
-    #data$ExpressedCount <- round_any(data$ExpressedCount, round_to)
+    # round off on x-axis to create bins
+    # TODO: this might be possible with ggplot
+    data$ExpressedCount <- round_any(data$ExpressedCount, round_to)
 
     error_width <- max_expr_count / 20
 
@@ -102,7 +104,7 @@ plot_degree_vs_expr <- function(ppi_name="bossi",expr_name="gene_atlas",prop_nam
 # 1.) tissue specific proteins make fewer protein interactions than widely
 #     expressed proteins (use spearman)
 # 2.) most recently evolved proteins have fewer interactions than ancient proteins TODO
-test_bossi_1 <- function(ppi_name="bossi", expr_name="gene_atlas", prop_name="maxts_degree")
+test_bossi_1 <- function(ppi_name="bossi", expr_name="gene_atlas", prop_name="coexpr_degree")
 {
     data <- get_expression_property_data(ppi_name, expr_name, prop_name)
 
@@ -110,29 +112,106 @@ test_bossi_1 <- function(ppi_name="bossi", expr_name="gene_atlas", prop_name="ma
 
     for (test in c("spearman", "kendall", "pearson"))
     {
-        tst = cor.test(data$ExpressedCount, data$Value, method=test)
-        rho = tst$estimate
-        pvalue = tst$p.value
-        print(paste("runing", test, "test: ", ppi_name, expr_name, "rho=", rho, ", p-value=", pvalue))
+        print(cor.test(data$ExpressedCount, data$Value, method=test))
+        #rho = tst$estimate
+        #pvalue = tst$p.value
+        #print(paste("runing", test, "test: ", ppi_name, expr_name, "rho=", rho, ", p-value=", pvalue))
     }
 }
+
+
+get_hkts_sym_threshold_table <- function(expr_name="gene_atlas", threshold=0.1)
+{
+    expr_count_table <- paste(expr_name, "expr_counts", sep='_')
+    # return top and bottom threshold percent as HK and TS (respectively)
+    hkts_table <- paste(
+                   "SELECT Gene, CASE ",
+                   "WHEN ExpressedCount*1.0/TotalCount <= ", threshold,
+                   " THEN 'TS'",
+                   "WHEN ExpressedCount*1.0/TotalCount >= 1.0 - ", threshold,
+                   " THEN 'HK'",
+                   "ELSE 'None' END AS Class",
+                   "FROM ", expr_count_table)
+    hkts_table <- paste("(", hkts_table, ")")
+    return (hkts_table)
+}
+
 
 # 3.) to which extend to tissue-specific proteins interact with most widely expressed proteins?
 test_bossi_3 <- function(ppi_name="bossi", expr_name="gene_atlas", prop_name="maxts_degree")
 {
-    # needs data from the actual graph -> TODO implement in NetworKit!?
+    # load the ts/hk summary data from the database
+    source("sql_config.R")
+    con <- get_sql_conn('/home/patrick/dev/bio/data/test_matching.sqlite')
+
+    expr_count_table <- paste(expr_name, "expr_counts", sep="_")
+    min_table <- paste("prop", ppi_name, expr_name, "min_neighbor_expr_count", sep="_")
+    max_table <- paste("prop", ppi_name, expr_name, "max_neighbor_expr_count", sep="_")
+    # Joining ExpressionCount with Min and Max
+    query <- paste("SELECT a.Gene, a.ExpressedCount, a.TotalCount, ",
+                   "b.Value AS Min, c.Value AS Max",
+                   "FROM ", expr_count_table, " AS a INNER JOIN ",
+                   min_table, " AS b ON a.Gene = b.Gene INNER JOIN ",
+                   max_table, " AS c ON a.Gene = c.Gene")
+                   #"WHERE a.ExpressedCount != 0")
+    data <- dbGetQuery(con, query)
+
+    nTissues <- max(data$TotalCount)
+
+    threshold <- 0.2
+
+    nTS <- sum(data$ExpressedCount <= threshold * nTissues)
+    n_ts_hk_neighbor <- sum(data$ExpressedCount <= threshold * nTissues & data$Max >= (1-threshold)*nTissues)
+
+    #print(paste(ppi_name, expr_name, ": ", n_ts_hk_neighbor, "/", nTS))
+
+    # actually do something with this data :)
+    # TODO for some reason there are many many Genes that are not expressed
+    #      in any cell !???
+    # this is especially the case for emtab, gene_atlas and rnaseq_atlas
+    # WHY THE FUCK?? might this be because of too high thresholds???
+
+    #return(data)
+    #return(data)
+    hist(data$ExpressedCount)
+    title(main=paste(ppi_name,expr_name))
+}
+
+
+
+get_hkts_edge_summary <- function(ppi_name="bossi", expr_name="gene_atlas", prop_name="maxts_degree")
+{
+    # this can be solved for various definitions of "housekeeping" and
+    # tissue specific proteins by only using SQL:
+
+    source("sql_config.R")
+    con <- get_sql_conn('/home/patrick/dev/bio/data/test_matching.sqlite')
+
+    # what's the definition of tissuespecific vs. housekeeping?
+    hkts_table <- get_hkts_sym_threshold_table(expr_name, 0.2)
+
+
+    # TODO (test): min(Class1,Class2),max(Class1,Class2)
+    query <- paste("SELECT MIN(a.Class,b.Class) AS Class1, MAX(a.Class, b.Class) AS Class2,",
+                   "COUNT(*) AS Count FROM ",
+                   ppi_name, " AS c INNER JOIN ",
+                   hkts_table, " AS a ON c.Gene1 = a.Gene INNER JOIN ",
+                   hkts_table, " AS b ON c.Gene2 = b.Gene",
+                   "GROUP BY Class1, Class2")
+    data <- dbGetQuery(con, query);
+    return (data)
 }
 
 
 create_test_degrees <- function(ppi_name="bossi",expr_name="gene_atlas",prop_name="degree_test")
 {
-    
+
     source("sql_config.R")
     con <- get_sql_conn('/home/patrick/dev/bio/data/test_matching.sqlite')
 
     # create table names
     prop_table <- paste("prop", ppi_name, expr_name, prop_name, sep="_")
-    
+
     dbSendQuery(con, paste("DROP TABLE IF EXISTS ", prop_table))
     query <- paste("CREATE TABLE IF NOT EXISTS ", prop_table, " AS ",
                    "SELECT Gene, COUNT(*) AS Value FROM ",
@@ -149,7 +228,7 @@ create_test_degrees <- function(ppi_name="bossi",expr_name="gene_atlas",prop_nam
 
 compare_test_degrees <- function(ppi_name="bossi",expr_name="gene_atlas",prop_name="degree_test")
 {
-    
+
     source("sql_config.R")
     con <- get_sql_conn('/home/patrick/dev/bio/data/test_matching.sqlite')
 
@@ -192,9 +271,9 @@ plot_all <- function()
         for (e in exprs)
         {
             # call the bossi plotting function
-            #fig <- plot_degree_vs_expr(p, e, "coexpr_degree")
+            fig <- plot_degree_vs_expr(p, e, "coexpr_degree")
             create_test_degrees(p,e);
-            fig <- plot_degree_vs_expr(p, e, "degree_test")
+            #fig <- plot_degree_vs_expr(p, e, "degree_test")
             figs <- c(figs, list(fig))
         }
     }
