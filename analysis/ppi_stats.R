c44be4974d077e6bbf1fbca6f2bac851698e1f13
r4d2 <patrick.flick@gmail.com>
2014-03-30 21:45:49 -0400

    global PPI graph properties calculated

---
+++ analysis/ppi_stats.R
@@ -0,0 +1,91 @@
+
+library(ggplot2)
+
+get_ppis <- function()
+{
+    # FIXME: do this properly (not hardcoded)
+    ppis <- c("bossi", "string", "psicquic_all", "havu", "ccsb")
+    return(ppis)
+}
+
+
+get_node_properties <- function(ppi_name = "string")
+{
+    # load the ts/hk summary data from the database
+    source("sql_config.R")
+    con <- get_sql_conn('/home/patrick/dev/bio/data/test_matching.sqlite')
+
+    ppi_node_prop_tbl <- paste(ppi_name, "node_properties", sep="_")
+    query <- paste("SELECT * FROM ", ppi_node_prop_tbl)
+    node_props <- dbGetQuery(con, query)
+
+    return(node_props)
+}
+
+get_graph_properties <- function(ppi_name="string")
+{
+    # load the ts/hk summary data from the database
+    source("sql_config.R")
+    con <- get_sql_conn('/home/patrick/dev/bio/data/test_matching.sqlite')
+
+    ppi_prop_tbl <- paste(ppi_name, "properties", sep="_")
+    query <- paste("SELECT * FROM ", ppi_prop_tbl)
+    props <- dbGetQuery(con, query)
+
+    return (props)
+}
+
+
+# TODO: print out graph information:
+#   - n
+#   - m
+#   - connected components
+#   - maximum degree
+#   - average degree
+#   - global clustering coeff
+#   - avg local clustering coeff
+#   - (?) degree assotativity 
+get_ppi_graph_stats <- function(ppi_name="string")
+{
+    # will get and/or calculate all graph properties and save them into
+    # named list elements
+    props = list()
+
+    # get the graph information from the database
+    graph_props <- get_graph_properties(ppi_name)
+    node_props <- get_node_properties(ppi_name)
+
+    # extract easy info
+    for (i in 1:dim(graph_props)[1])
+    {
+        props[[graph_props[i,1]]] = graph_props[i,2]
+    }
+
+    # TODO: summarize node wise data
+
+
+    # return result
+    return(props)
+}
+
+
+ppi_stats_table <- function()
+{
+    for (p in get_ppis())
+    {
+        stats <- get_ppi_graph_stats(p)
+        # todo output as table! (i.e. somehow reformat as data frame and then
+        # output as LaTeX table with the columns that i want!
+        print(stats)
+    }
+}
+
+
+# TODO: degree distribution fit
+#   - print fit of degree distr to binomial, poisson and scale-free
+#   - and according distribution parameters
+#   - maybe plot showing the different degree distributions and the real one
+#     (to show the actual goodness of fit)
+
+
+# TODO: plot clustering coeff against p (N-1) [ see barabasi book ]
---
+++ src/graph_properties.py
@@ -0,0 +1,191 @@
+# this file is for interactive tests
+# thus it just loads the configuration and
+# database connection and enables tab completion
+
+import os
+import re
+import itertools
+import numpy
+import time
+import sys
+
+import pappi.id_mapping
+import pappi.sql
+from pappi.data_config import *
+
+
+###################################
+#  get and save graph properties  #
+###################################
+
+def save_node_properties(graph_name, gene_names, properties, sql_conn):
+    for prop_name, props in properties.items():
+        if len(gene_names) != len(props):
+            raise Exception()
+
+    # initialize the cursor object
+    cur = sql_conn.cursor()
+
+    # create the properties table
+    table = graph_name + "_node_properties"
+    props = list(properties.keys())
+    cur.execute('DROP TABLE IF EXISTS "' + table + '"')
+    cur.execute('CREATE TABLE "' + table + '" ("Gene" varchar(16), '
+                + ', '.join('"' + p + '" REAL' for p in props) + ');')
+
+    # fill the properties table
+    for i in range(0, len(gene_names)):
+        prop_fields = ', '.join(props)
+        values = tuple([gene_names[i]] + [properties[p][i] for p in props])
+        cur.execute('INSERT INTO "' + table + '" (Gene, ' + prop_fields
+                    + ') VALUES (?,' + ','.join('?'*len(props)) + ')',
+                    values)
+
+    # cluse cursor and commit changes
+    cur.close()
+    sql_conn.commit()
+
+
+def get_node_properties(ppi, con, verbose=True):
+    props = dict()
+    if verbose:
+        print("getting degrees")
+    props['degree'] = ppi.getDegrees()
+
+    g = ppi.getGraph()
+
+    # calculate betweenness
+    if verbose:
+        print("getting graph betweenness")
+    between = ppi_networkit.Betweenness(g)
+    between.run()
+    props['Betweenness'] = between.scores()
+
+    # calculate clustering coeff
+    if verbose:
+        print("getting graph clustering coeff")
+    clustercoef = ppi_networkit.ClusteringCoefficient()
+    props['ClusteringCoeff'] = clustercoef.exactLocal(g)
+
+    # save the node properties to the database
+    if verbose:
+        print("saving node properties")
+    ppi_name = ppi.getPpiName()
+    save_node_properties(ppi_name, ppi.getAllGenes(), props, con)
+
+
+def save_graph_properties(graph_name, props, sql_conn):
+    # initialize the cursor object
+    cur = sql_conn.cursor()
+
+    # create the properties table
+    table = graph_name + "_properties"
+    cur.execute('DROP TABLE IF EXISTS "' + table + '"')
+    cur.execute('CREATE TABLE "' + table + '" ("Property" varchar(16), '
+                '"Value" REAL)')
+
+    # for every property:
+    for p, val in props.items():
+        cur.execute('INSERT INTO "' + table + '" (Property,Value) '
+                    'VALUES (?,?)', (p, val))
+    # close connection and commit changes
+    cur.close()
+    sql_conn.commit()
+
+
+def get_graph_properties(ppi, con, verbose=True):
+    g = ppi.getGraph()
+
+    props = dict()
+
+    if verbose:
+        print("get basic graph info")
+    props['n'] = g.numberOfNodes()
+    props['m'] = g.numberOfEdges()
+
+    if verbose:
+        print("get degree stats")
+    gp = ppi_networkit.GraphProperties
+    minmax = gp.minMaxDegree(g)
+    props['min_deg'] = minmax[0]
+    props['max_deg'] = minmax[1]
+    props['avg_deg'] = gp.averageDegree(g)
+    props['deg_assortativity'] = gp.degreeAssortativity(g, False)
+
+    if verbose:
+        print("get connected components")
+    cc = ppi_networkit.ConnectedComponents()
+    cc.run(g)
+    props['conn_comp'] = cc.numberOfComponents()
+
+    if verbose:
+        print("get global clustering coeff")
+    clc = ppi_networkit.ClusteringCoefficient()
+    props['gl_cluster_coeff'] = clc.exactGlobal(g)
+
+    if verbose:
+        print("get diameter")
+    if props['conn_comp'] == 1:
+        dm = ppi_networkit.Diameter
+        props['diameter'] = dm.exactDiameter(g)
+    else:
+        print("    ! graph not connected -> setting diameter = 0")
+        props['diameter'] = 0
+
+    ppi_name = ppi.getPpiName()
+    save_graph_properties(ppi_name, props, con)
+
+
+#############################
+#  get database connection  #
+#############################
+
+# get new database connection
+con = pappi.sql.get_conn(DATABASE)
+
+
+#################################
+#  Import PPI NetworKit module  #
+#################################
+
+#sys.path.append("/home/patrick/dev/bio/NetworKit-Flick/cython")
+sys.path.append("/home/patrick/dev/bio/ppi_networkit/cython")
+#import NetworKit
+import ppi_networkit
+# set the log-level to "ERROR", this will ignore the [INFO] and [WARN] logs
+ppi_networkit.setLogLevel("ERROR")
+sqlio = ppi_networkit.SQLiteIO(DATABASE)
+
+
+#################################
+#  Get global-graph properties  #
+#################################
+
+for ppi_name in ["string", "ccsb", "bossi", "psicquic_all", "havu"]:
+    print()
+    print("##################################################")
+    print(" getting graph properties of `" + ppi_name + "`")
+    print("##################################################")
+    ppi = sqlio.load_ppi_graph(ppi_name)
+    get_node_properties(ppi, con)
+    get_graph_properties(ppi, con)
+
+##########################################
+#  Get tissue-specific graph properties  #
+##########################################
+
+#ppi = "ccsb"
+#ppi = "bossi"
+#expr = "hpa"
+#expr = "hpa_all"
+#tsppi = sqlio.load_tsppi_graph(ppi, expr)
+#g = tsppi.getGraph()
+#
+
+################################
+#  enable for interactive use  #
+################################
+
+import readline
+import rlcompleter
+readline.parse_and_bind("tab: complete")
