09c5a96e1c1bc32db7b7f04f9e1580b11683084e
r4d2 <patrick.flick@gmail.com>
2014-04-21 21:25:32 -0400

    adding timing to graph properties

--- src/graph_properties.py
+++ src/graph_properties.py
@@ -46,32 +46,41 @@ def save_node_properties(graph_name, gene_names, properties, sql_conn):
     sql_conn.commit()
 
 
-def get_node_properties(ppi, con, verbose=True):
+def get_node_properties(ppi, con, verbose=True, timings={}):
+    # get name
+    ppi_name = ppi.getPpiName()
+
     props = dict()
     if verbose:
         print("getting degrees")
+    t = time.time()
     props['degree'] = ppi.getDegrees()
+    timings[(ppi_name,'degrees')] = time.time() - t
 
     g = ppi.getGraph()
 
     # calculate betweenness
     if verbose:
         print("getting graph betweenness")
+    t = time.time()
     between = ppi_networkit.Betweenness(g)
     between.run()
     props['Betweenness'] = between.scores()
+    timings[(ppi_name,'betweenness')] = time.time() - t
 
     # calculate clustering coeff
     if verbose:
         print("getting graph clustering coeff")
+    t = time.time()
     clustercoef = ppi_networkit.ClusteringCoefficient()
     props['ClusteringCoeff'] = clustercoef.exactLocal(g)
+    timings[(ppi_name, 'clusteringcoeff')] = time.time() - t
 
     # save the node properties to the database
     if verbose:
         print("saving node properties")
-    ppi_name = ppi.getPpiName()
-    save_node_properties(ppi_name, ppi.getAllGenes(), props, con)
+    #save_node_properties(ppi_name, ppi.getAllGenes(), props, con)
+    return timings
 
 
 def save_graph_properties(graph_name, props, sql_conn):
@@ -93,47 +102,59 @@ def save_graph_properties(graph_name, props, sql_conn):
     sql_conn.commit()
 
 
-def get_graph_properties(ppi, con, verbose=True):
+def get_graph_properties(ppi, con, verbose=True, timings={}):
+    # get graph and name
     g = ppi.getGraph()
+    ppi_name = ppi.getPpiName()
 
     props = dict()
 
     if verbose:
         print("get basic graph info")
+    t = time.time()
     props['n'] = g.numberOfNodes()
     props['m'] = g.numberOfEdges()
+    timings[(ppi_name, "size")] = time.time() - t
 
     if verbose:
         print("get degree stats")
+    t = time.time()
     gp = ppi_networkit.GraphProperties
     minmax = gp.minMaxDegree(g)
     props['min_deg'] = minmax[0]
     props['max_deg'] = minmax[1]
     props['avg_deg'] = gp.averageDegree(g)
     props['deg_assortativity'] = gp.degreeAssortativity(g, False)
+    timings[(ppi_name, "degree")] = time.time() - t
 
     if verbose:
         print("get connected components")
+    t = time.time()
     cc = ppi_networkit.ConnectedComponents()
     cc.run(g)
     props['conn_comp'] = cc.numberOfComponents()
+    timings[(ppi_name, "conn_comp")] = time.time() - t
 
     if verbose:
         print("get global clustering coeff")
+    t = time.time()
     clc = ppi_networkit.ClusteringCoefficient()
     props['gl_cluster_coeff'] = clc.exactGlobal(g)
+    timings[(ppi_name, "gl_cluster_coeff")] = time.time() - t
+
 
     if verbose:
         print("get diameter")
+    t = time.time()
     if props['conn_comp'] == 1:
         dm = ppi_networkit.Diameter
         props['diameter'] = dm.exactDiameter(g)
     else:
         print("    ! graph not connected -> setting diameter = 0")
         props['diameter'] = 0
-
-    ppi_name = ppi.getPpiName()
-    save_graph_properties(ppi_name, props, con)
+    timings[(ppi_name, "diameter")] = time.time() - t
+    #save_graph_properties(ppi_name, props, con)
+    return timings
 
 
 #############################
@@ -161,14 +182,21 @@ sqlio = ppi_networkit.SQLiteIO(DATABASE)
 #  Get global-graph properties  #
 #################################
 
+timings = {}
 for ppi_name in ["string", "ccsb", "bossi", "psicquic_all", "havu"]:
     print()
     print("##################################################")
     print(" getting graph properties of `" + ppi_name + "`")
     print("##################################################")
     ppi = sqlio.load_ppi_graph(ppi_name)
-    get_node_properties(ppi, con)
-    get_graph_properties(ppi, con)
+    timings = get_node_properties(ppi, con, True, timings)
+    timings = get_graph_properties(ppi, con, True, timings)
+
+print()
+print("##################################################")
+print("         time taken by each part")
+print("##################################################")
+print(timings)
 
 ##########################################
 #  Get tissue-specific graph properties  #
