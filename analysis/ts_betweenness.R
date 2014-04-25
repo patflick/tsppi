eca56c09bc395c6d8cf28170cc8a263d07d47201
r4d2 <patrick.flick@gmail.com>
2014-04-25 00:39:51 -0400

    tsppi basic analysis

--- analysis/ts_betweenness.R
+++ analysis/ts_betweenness.R
@@ -2,12 +2,12 @@
 library(ggplot2)
 library(xtable) # for converting a data frame into a latex table
 library(gridExtra) # for `grid.arrange`
-library(igraph) # for power.law.fit
 library(scales) # for muted
 
 
 source("ppi_utils.R")
 source("expr_utils.R")
+source("plot_tiles.R")
 
 get_exprs <- function()
 {
@@ -259,51 +259,6 @@ hk_ts_all_scores <- function(threshold=0.1, HK=TRUE, agg_func=NA)
     return (df)
 }
 
-# TODO: this is almost identical to the function in bossi_lehner.R
-#       -> generalize it somewhere!
-plot_tiles_for_ppi_expr <- function(plot_data, font_size=4)
-{
-
-    # create labels if they don't exist yet
-    if (length(plot_data$label) == 0)
-    {
-
-        if (max(plot_data$value) <= 1.0)
-        {
-            plot_data$value <- plot_data$value * 100.0
-        }
-
-        # generate labels (adding the sizes to the empty rows/cols
-        plot_data$label = paste(round(plot_data$value, 1), " %", sep="")
-    }
-
-    # translate the db aliases into the real names of PPIs and Expression sets
-    plot_data$expr <- to_short_expr_name(plot_data$expr)
-    plot_data$ppi <- to_short_ppi_name(plot_data$ppi)
-
-    # plot as heatmap table
-    # TODO generalize the heatmap table into a separate function
-    p <- ggplot(plot_data, aes(expr, ppi)) +
-            geom_tile(aes(fill=value,label=label,colour=value)) +
-            # fill colors:
-            scale_fill_gradient2("Percent", midpoint=50, limits=c(0,100)) +
-            # font colors:
-            scale_color_gradient2(low="grey40", mid="black", high="black", limits=c(0,100), guide='none') +
-            # set labels:
-            # TODO set size here with parameter
-            geom_text(aes(label=label, colour=value), size=font_size) +
-            xlab('') + ylab('') +
-            theme(# disable grid and axis ticks
-                  panel.grid.major=element_blank(),
-                  panel.grid.minor=element_blank(),
-                  axis.ticks=element_blank(),
-                  panel.background=element_blank(),
-                  # flip x axis
-                  #axis.text.x=element_text(angle=90, hjust=1)
-                  axis.text.x=element_text(size=10)
-                  )
-    return(p)
-}
 
 agg_summary <- function(data)
 {
--- analysis/tsppi_stats.R
+++ analysis/tsppi_stats.R
@@ -1,15 +1,14 @@
+library(scales) # for muted and alpha
 
 source("ppi_utils.R")
 source("expr_utils.R")
+source("plot_tiles.R")
 
 get_exprs <- function()
 {
     exprs <- c("emtab", "gene_atlas", "rnaseq_atlas", "hpa", "hpa_all")
     return(exprs)
 }
-
-
-
 
 get_graph_properties <- function(ppi_name="string", expr_name="gene_atlas")
 {
@@ -88,4 +87,61 @@ get_merged_properties <- function()
     return(z)
 }
 
-# TODO: plot and percentages into grid plot
+# average size of subnetworks
+plot_size_perc <- function()
+{
+    # get data
+    data <- get_merged_properties()
+    # get percentage of original size
+    data$value <- data$ts_m * 100 / data$m
+    data$label <- paste(round(data$value, 1), "%")
+
+    # plot into tiles
+    p <- plot_tiles_for_ppi_expr(data)
+    return (p)
+}
+
+plot_connected_comp <- function()
+{
+    data <- get_merged_properties()
+    # get percentage of original size
+    data$value <- data$ts_conn_comp / data$conn_comp
+    data$label <- paste(round(data$value, 1), "x")
+
+    # plot into tiles
+    p <- plot_tiles_for_ppi_expr(data)
+    p <- p + scale_fill_gradient2("Factor increase",na.value=alpha(muted("red"),0.9),midpoint=15, mid=alpha(muted("red"),0.5),high=alpha(muted("red"),0.9), low="white", limits=c(0, 30))
+    return (p)
+}
+
+plot_cluster_coeff <- function()
+{
+    data <- get_merged_properties()
+    # get percentage of original size
+    data$value <- data$ts_gl_cluster_coeff * 100 / data$gl_cluster_coeff
+    data$label <- paste(round(data$value, 1), "%")
+
+    # plot into tiles
+    p <- plot_tiles_for_ppi_expr(data)
+    p <- p + scale_fill_gradient2("Percentage change",na.value=alpha(muted("blue"),0.9),midpoint=100, mid="white",high=alpha(muted("blue"),0.9), low=alpha(muted("red"),0.9), limits=c(50, 150))
+    p <- p + scale_color_gradient2(low="black", mid="black", high="black", na.value="black", guide='none', limits=c(0,500))
+    return (p)
+}
+
+size_tiles <- function()
+{
+    pdf(paste("../figs/tsppi_size_diff.pdf", sep=""), width=7, height=3)
+    p <- plot_size_perc()
+    p <- p + labs(title="Average sizes of TS subnetworks")
+    plot(p)
+    dev.off()
+}
+
+gcc_tiles <- function()
+{
+    pdf(paste("../figs/tsppi_gcc_diff.pdf", sep=""), width=7, height=3)
+    p <- plot_cluster_coeff()
+    p <- p + labs(title="Average clustering coeff. of TS subnetworks")
+    plot(p)
+    dev.off()
+}
