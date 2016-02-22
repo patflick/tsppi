13a3bcbcc2d25c6569225e6824e10a360508e76c
patflick <patrick.flick@gmail.com>
2016-02-22 12:58:10 -0500

    fix timer for Mac OS

--- ppi_networkit/src/cputimer.h
+++ ppi_networkit/src/cputimer.h
@@ -16,6 +16,11 @@
 
 #include <time.h>
 #include <stdint.h>
+// enable time measurements on MAC OS
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
 
 class CPUTimer {
 
@@ -25,7 +30,17 @@ class CPUTimer {
 
         inline long long microsecsonds_ts() {
             struct timespec ts;
+#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
+            clock_serv_t cclock;
+            mach_timespec_t mts;
+            host_get_clock_service(mach_host_self(), SYSTEM_CLOCK, &cclock);
+            clock_get_time(cclock, &mts);
+            mach_port_deallocate(mach_task_self(), cclock);
+            ts->tv_sec = mts.tv_sec;
+            ts->tv_nsec = mts.tv_nsec;
+#else
             clock_gettime(CLOCK_MONOTONIC, &ts);
+#endif
             return (uint64_t)ts.tv_sec * 1000000LL + (uint64_t)ts.tv_nsec / 1000LL;
         }
     public:
